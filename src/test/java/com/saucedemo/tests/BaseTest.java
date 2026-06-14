ece1435980adbea3ebad03760631d9ef25c96bc6
Ashik BR <ashikbr79@gmail.com>
2026-06-14 12:32:56 +0530

    Added TestNG browser parameterization support

--- src/test/java/com/saucedemo/tests/BaseTest.java
+++ src/test/java/com/saucedemo/tests/BaseTest.java
@@ -4,14 +4,16 @@ import com.saucedemo.factory.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
+    @Parameters("browser")
+    public void setup(String browser) {
 
-        driver = DriverFactory.initDriver("chrome");
+        driver = DriverFactory.initDriver(browser);
         driver.get("https://www.saucedemo.com/");
     }
 
