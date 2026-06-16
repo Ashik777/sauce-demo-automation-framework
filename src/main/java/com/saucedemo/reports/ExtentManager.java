07b7158da820b6b67b21763f6658b0dc83ecb291
Ashik BR <ashikbr79@gmail.com>
2026-06-16 11:01:10 +0530

    Add Extent Reports integration with TestNG listener

---
+++ src/main/java/com/saucedemo/reports/ExtentManager.java
@@ -0,0 +1,18 @@
+package com.saucedemo.reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+    public static ExtentReports getExtendReports()
+    {
+        if(extent==null)
+        {
+            ExtentSparkReporter spark= new ExtentSparkReporter("test-output/ExtentReport.html");
+            extent=new ExtentReports();
+            extent.attachReporter(spark);
+        }
+        return extent;
+    }
+}
