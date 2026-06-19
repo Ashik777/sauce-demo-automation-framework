2c356886cbbb6dcab511e0243a15a125599345db
Ashik BR <ashikbr79@gmail.com>
2026-06-19 14:27:25 +0530

    added properties file

---
+++ src/main/java/com/saucedemo/utilities/ConfigReader.java
@@ -0,0 +1,26 @@
+package com.saucedemo.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties prop;
+    static{
+        try
+        {
+            FileInputStream input= new FileInputStream("src/test/resources/config.properties");
+            prop=new Properties();
+            prop.load(input);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+    public static String getProperty(String key)
+    {
+        return prop.getProperty(key);
+    }
+}
